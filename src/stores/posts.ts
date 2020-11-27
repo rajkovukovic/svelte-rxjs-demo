8797b2dfafc1149d9c22da7f448acb5e1e3b63b0
Rajko Vukovic <rajkovukovic@gmail.com>
2020-11-27 23:08:40 +0100

    make it simple

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,36 +1,28 @@
-import { combineLatest, Observable, of } from "rxjs";
-import { distinctUntilChanged, shareReplay, switchMap, tap } from "rxjs/operators";
-import { fromFetch } from 'rxjs/fetch';
+import { Observable, of } from "rxjs";
+import { shareReplay, switchMap, tap } from "rxjs/operators";
 import { Logger } from "./logs";
-import { userIdStore, userStore } from "./user";
+import { userStore } from "./user";
 
 function fetchPosts(userId: String) {
-  return fromFetch(`https://jsonplaceholder.typicode.com/posts/?userId=${userId}`).pipe(switchMap(response => {
-    if (response.ok) {
-      // OK return data
-      return response.json();
-    } else {
-      // Server is returning a status requiring the client to try something else.
-      return of({ error: true, message: `Error ${response.status}` });
-    }
-  }));
+  return fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${userId}`).then(response => response.json());
 }
 
-export const postsStore: Observable<Object[] | null> = combineLatest([userIdStore, userStore]).pipe(
-  switchMap(([userId, user]) => {
-    if (userId && userId === user?.id) {
+export const postsStore: Observable<Object[] | null> = userStore.pipe(
+  switchMap((user) => {
+    if (user) {
       Logger.logWithColor('rgb(180,0,100)', `Fetching Posts for User "${user.id}"`);
       return fetchPosts(user.id);
     }
 
     return of(null);
   }),
-  distinctUntilChanged(),
+
   tap(posts => {
     if (posts)
       Logger.logWithColor('rgb(255,0,140)', `Posts for User "${posts[0].userId}" fetched successfully`);
     else
       Logger.log(`Posts are now "null"`);
   }),
+
   shareReplay(1),
 );
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,38 +1,29 @@
-import { BehaviorSubject, of, merge } from "rxjs";
-import { fromFetch } from "rxjs/fetch";
-import { distinctUntilChanged, shareReplay, switchMap, tap } from "rxjs/operators";
+import { BehaviorSubject, of } from "rxjs";
+import { shareReplay, switchMap, tap } from "rxjs/operators";
 import { Logger } from "./logs";
 
 function fetchUser(id: String) {
-  return fromFetch(`https://jsonplaceholder.typicode.com/users/${id}`).pipe(switchMap(response => {
-    if (response.ok) {
-      // OK return data
-      return response.json();
-    } else {
-      // Server is returning a status requiring the client to try something else.
-      return of({ error: true, message: `Error ${response.status}` });
-    }
-  }));
+  return fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(response => response.json());
 }
 
 export const userIdStore = new BehaviorSubject<string | null>(null);
-// export const isUserLoadingStore = new BehaviorSubject<boolean>(false);
 
 export const userStore = userIdStore.pipe(
   switchMap(id => {
     if (id) {
       Logger.logWithColor('rgb(0,100,180)', `Fetching User "${id}"`);
-      return merge(of(null), fetchUser(id));
+      return fetchUser(id);
     }
 
     return of(null);
   }),
-  distinctUntilChanged(),
+
   tap(user => {
     if (user)
       Logger.logWithColor('rgb(0,140,255)', `User "${user.id}" fetched successfully`);
     else
       Logger.log(`User is now "null"`);
   }),
+
   shareReplay(1),
 );
